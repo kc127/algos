26ec9fd4ef56b6e48586c3e898896a6957ac0515
Kanchan Chauhan <kanchan827@gmail.com>
2025-10-29 20:22:57 -0400

    pathSum

--- trees/pathSum.js
+++ trees/pathSum.js
@@ -12,7 +12,6 @@
  * @return {boolean}
  */
 var hasPathSum = function(root, targetSum) {
-  let pathSum = [];
 
   function dfs(node, currSum = 0) {
       if (!node) {
@@ -25,4 +24,29 @@ var hasPathSum = function(root, targetSum) {
       return dfs(node.left, currSum) || dfs(node.right, currSum);
   }
   return dfs(root)
+};
+
+// Approach 2
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+
+  if (!root) return false;
+  targetSum -= root.val;
+  if (!root.left && !root.right) {
+       if (targetSum === 0) return true;
+  }
+
+  return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
 };
