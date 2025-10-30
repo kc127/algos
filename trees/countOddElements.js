fb32a43bc9e16a15a0ce4f755f2614862b456ee1
Kanchan Chauhan <kanchan827@gmail.com>
2025-10-29 22:48:47 -0400

    count odd elems

--- trees/countOddElements.js
+++ trees/countOddElements.js
@@ -1,25 +1,56 @@
-const isOdd = (x) => (x % 2 === 1);
-
-function countOdd(root) {
-
+function countOddNodesRecursive (root) {
   let count = 0;
 
   function dfs (node) {
-    if (!node) return 0;
-
-    if (isOdd(node.value)) count++;
+    if (!node) return;
+    if (node.val % 2 === 1) count++;
 
     dfs(node.left);
     dfs(node.right);
   }
+
   dfs(root);
   return count;
 }
 
+function countOddNodesIterative (root) {
+  if (!root) return 0;
+
+  let result = 0;
+  let stack = root ? [root] : [];
+
+  while (stack.length > 0) {
+    let curr = stack.pop();
+    if (curr.val % 2 === 1) {
+      result++;
+    }
+    if (curr.left) {
+      stack.push(curr.left);
+    }
+    if (curr.right) {
+      stack.push(curr.right);
+    }
+  }
+  return result;
+}
+
 class TreeNode {
-  constructor (value, left = null, right = null) {
-    this.value = value;
+  constructor (val, left = null, right = null) {
+    this.val = val;
     this.left = left;
     this.right = right;
   }
 }
+
+let treeA = new TreeNode(10,
+              new TreeNode(5,
+                new TreeNode(3),
+                new TreeNode(7)
+              ),
+              new TreeNode(15,
+                null,
+                new TreeNode(18)
+              )
+);
+
+console.log(countOddNodesIterative(treeA),' expect 4');
