6ef43da69945646937645fa3e72af9d2f70772e9
Kanchan Chauhan <kanchan827@gmail.com>
2025-11-17 17:44:43 -0500

    valid palindrome II

--- twoPointers/validPalindromeII.js
+++ twoPointers/validPalindromeII.js
@@ -1,27 +1,31 @@
 /**
  * https://leetcode.com/problems/valid-palindrome-ii/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
   */
- function validPalindrome (s) {
+ /**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
   let i = 0;
   let j = s.length - 1;
 
   while (i <= j) {
-    if (s.charAt(i) !== s.charAt(j)) {
-      return isPalindrome(s, i, j - 1) || isPalindrome(s, i - 1, j);
-    }
-    i++;
-    j--;
+      if (s.charAt(i) !== s.charAt(j)) {
+          return isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j);
+      }
+      i++;
+      j--;
   }
   return true;
- }
+};
 
- function isPalindrome(str, i, j) {
+function isPalindrome (str, i, j) {
   while (i <= j) {
-    if (str[i] !== str[j]) {
-      return false;
-    }
-    i++;
-    j--;
+      if (str.charAt(i) !== str.charAt(j)) {
+          return false;
+      }
+      i++;
+      j--;
   }
   return true;
- }
+}
