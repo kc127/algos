4677916f1bba7831ee6857897191235dba162eb2
Kanchan Chauhan <kanchan827@gmail.com>
2025-11-19 15:04:49 -0500

    reverse linked list

---
+++ linkedlist/reverseLinkedList.js
@@ -0,0 +1,14 @@
+var reverseList = function(head) {
+  if (!head) return null;
+
+  let prev = null;
+  let curr = head;
+
+  while (curr) {
+      let temp = curr.next;
+      curr.next = prev;
+      prev = curr
+      curr = temp;
+  }
+  return prev;
+};
