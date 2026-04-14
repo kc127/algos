ba77470bd23803cd8c0726c8e0209f33070ac169
Kanchan Chauhan <kanchan827@gmail.com>
2026-04-14 10:57:00 -0400

    rotateArray

---
+++ twoPointers/rotateArray.js
@@ -0,0 +1,15 @@
+function rotateArray (nums, k) {
+  k = k % nums.length;
+
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+}
+
+function reverse(nums, left, right) {
+  while (left <= right) {
+    [nums[left], nums[right]] = [nums[right], nums[left]];
+    left++;
+    right--;
+  }
+}
