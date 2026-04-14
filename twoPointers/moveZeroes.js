928fe59f87b2ca9adf3b84bef3de876b7f2e039c
Kanchan Chauhan <kanchan827@gmail.com>
2026-04-14 10:47:42 -0400

    move zeroes

--- twoPointers/moveZeroes.js
+++ twoPointers/moveZeroes.js
@@ -17,3 +17,21 @@ function moveZeroes(arr) {
   }
   return arr;
 }
+
+// approach 2: simpler + optimal
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let i = 0;
+  let j = 0;
+
+  while (i < nums.length && j < nums.length) {
+      if (nums[j] !== 0) {
+          [nums[i], nums[j]] = [nums[j], nums[i]];
+          i++;
+      }
+      j++;
+  }
+};
